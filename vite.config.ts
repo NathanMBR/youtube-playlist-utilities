94fa8a13b700c9ed05e13cc869a3d731bdc0d32f
Nathan Murillo <nathanmurillodeoliveira@gmail.com>
2023-09-02 00:31:22 -0300

    chore: make vite.config.ts export its settings

--- vite.config.ts
+++ vite.config.ts
@@ -1,34 +1,34 @@
-import { defineConfig } from "vite";
+import { defineConfig, UserConfig } from "vite";
 import react from "@vitejs/plugin-react-swc";
 import path from "node:path";
 
+export const viteConfig: UserConfig = {
+  root: "./src/main/vite",
+  plugins: [react()],
+  resolve: {
+    alias: [
+      {
+        find: "@",
+        replacement: path.resolve(__dirname, "./src")
+      }
+    ]
+  },
+
+  // Vite options tailored for Tauri development and only applied in `tauri dev` or `tauri build`
+  //
+  // 1. prevent vite from obscuring rust errors
+  clearScreen: false,
+  // 2. tauri expects a fixed port, fail if that port is not available
+  server: {
+    port: 1420,
+    strictPort: true
+  },
+  // 3. to make use of `TAURI_DEBUG` and other env variables
+  // https://tauri.studio/v1/api/config#buildconfig.beforedevcommand
+  envPrefix: ["VITE_", "TAURI_"]
+};
+
 // https://vitejs.dev/config/
 export default defineConfig(
-  async () => {
-    return {
-      root: "./src/main/vite",
-      plugins: [react()],
-      resolve: {
-        alias: [
-          {
-            find: "@",
-            replacement: path.resolve(__dirname, "./src")
-          }
-        ]
-      },
-
-      // Vite options tailored for Tauri development and only applied in `tauri dev` or `tauri build`
-      //
-      // 1. prevent vite from obscuring rust errors
-      clearScreen: false,
-      // 2. tauri expects a fixed port, fail if that port is not available
-      server: {
-        port: 1420,
-        strictPort: true
-      },
-      // 3. to make use of `TAURI_DEBUG` and other env variables
-      // https://tauri.studio/v1/api/config#buildconfig.beforedevcommand
-      envPrefix: ["VITE_", "TAURI_"]
-    };
-  }
+  async () => viteConfig
 );
