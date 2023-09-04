88442e4bf69a9263e9822d92859ef96fdf6e25b1
Nathan Murillo <nathanmurillodeoliveira@gmail.com>
2023-09-04 12:19:19 -0300

    feat: add VALIDATION_FAILED to GetNonPublicVideos error response

--- src/domain/usecases/GetNonPublicVideos.ts
+++ src/domain/usecases/GetNonPublicVideos.ts
@@ -12,7 +12,10 @@ export namespace GetNonPublicVideos {
     data: NonPublicVideos;
   }
 
-  type FailureResponseErrors = "INVALID_PLAYLIST_URL" | "REPOSITORY_FAILED";
+  type FailureResponseErrors =
+    "VALIDATION_FAILED" |
+    "INVALID_PLAYLIST_URL" |
+    "REPOSITORY_FAILED";
 
   type FailureResponse = {
     success: false;
