63b8c12f22f92f020e67484b22ac18c93d5e248f
Nathan Murillo <nathanmurillodeoliveira@gmail.com>
2023-09-04 12:31:02 -0300

    feat: make GetNonPublicVideosImpl use validation

--- src/data/usecases/video/GetNonPublicVideosImpl.ts
+++ src/data/usecases/video/GetNonPublicVideosImpl.ts
@@ -1,13 +1,23 @@
 import { GetNonPublicVideos } from "@/domain/usecases";
+import { GetNonPublicVideosValidator } from "@/data/validators";
 import { GetPlaylistVideosRepository } from "@/data/repositories";
 
 export class GetNonPublicVideosImpl implements GetNonPublicVideos {
   constructor(
+    private readonly getNonPublicVideosValidator: GetNonPublicVideosValidator,
     private readonly getPlaylistVideosRepository: GetPlaylistVideosRepository
   ) {}
 
   async execute(request: GetNonPublicVideos.Request): GetNonPublicVideos.Response {
-    const { playlistURL } = request;
+    const validationResponse = this.getNonPublicVideosValidator.validate(request);
+
+    if (!validationResponse.success)
+      return {
+        success: false,
+        error: "VALIDATION_FAILED"
+      };
+
+    const { playlistURL } = validationResponse.data;
 
     const playlistIdParsed = playlistURL.split("list=")[1];
     if (!playlistIdParsed)
