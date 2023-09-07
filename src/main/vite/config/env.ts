e052dbe42fb9811aff66eb3d34caf8ac638270b7
Nathan Murillo <nathanmurillodeoliveira@gmail.com>
2023-09-07 11:32:14 -0300

    feat: export GOOGLE_OAUTH_BASE_URL

--- src/main/vite/config/env.ts
+++ src/main/vite/config/env.ts
@@ -1,2 +1,3 @@
 export const GITHUB_REPOSITORY_URL = String(import.meta.env.VITE_GITHUB_REPOSITORY_URL);
 export const GOOGLE_OAUTH_CLIENT_ID = String(import.meta.env.VITE_GOOGLE_OAUTH_CLIENT_ID);
+export const GOOGLE_OAUTH_BASE_URL = String(import.meta.env.VITE_GOOGLE_OAUTH_BASE_URL);
