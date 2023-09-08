34e35bbcece92cf589a3e3b5d2b46da386aecf7e
Nathan Murillo <nathanmurillodeoliveira@gmail.com>
2023-09-08 20:32:35 -0300

    feat: provide mantine style

--- src/main/vite/App.tsx
+++ src/main/vite/App.tsx
@@ -1,8 +1,37 @@
+import {
+  ColorSchemeProvider,
+  MantineProvider,
+  ColorScheme
+} from "@mantine/core";
+import { useLocalStorage } from "@mantine/hooks";
+
 export const App = () => {
+  const [colorScheme, setColorScheme] = useLocalStorage<ColorScheme>(
+    {
+      key: "theme",
+      defaultValue: "light",
+      getInitialValueInEffect: true
+    }
+  );
+
+  const toggleColorScheme = (value?: ColorScheme) => setColorScheme(value || (colorScheme === "dark" ? "light" : "dark"));
+
   return (
     <>
-      <h1>YouTube Playlist Utilities</h1>
-      <p>Under construction...</p>
+      <ColorSchemeProvider
+        colorScheme={colorScheme}
+        toggleColorScheme={toggleColorScheme}
+      >
+        <MantineProvider
+          theme={{ colorScheme }}
+          withGlobalStyles
+          withNormalizeCSS
+        >
+          <h1>YouTube Playlist Utilities</h1>
+          <p>Under construction...</p>
+          <button onClick={() => toggleColorScheme()}>Toggle theme</button>
+        </MantineProvider>
+      </ColorSchemeProvider>
     </>
   );
 };
