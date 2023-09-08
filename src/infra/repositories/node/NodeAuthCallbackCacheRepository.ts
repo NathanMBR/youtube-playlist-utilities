b8aaf01a1d29418bfa9e060c56ea76d46a664558
Nathan Murillo <nathanmurillodeoliveira@gmail.com>
2023-09-08 20:09:55 -0300

    feat: add set method to NodeAuthCallbackCacheRepository

--- src/infra/repositories/node/NodeAuthCallbackCacheRepository.ts
+++ src/infra/repositories/node/NodeAuthCallbackCacheRepository.ts
@@ -1,6 +1,12 @@
-import { GetAuthCallbackCacheRepository } from "@/data/repositories";
+import {
+  GetAuthCallbackCacheRepository,
+  SetAuthCallbackCacheRepository
+} from "@/data/repositories";
 
-export class NodeAuthCallbackCacheRepository implements GetAuthCallbackCacheRepository {
+export class NodeAuthCallbackCacheRepository implements
+  GetAuthCallbackCacheRepository,
+  SetAuthCallbackCacheRepository
+{
   get(): GetAuthCallbackCacheRepository.Response {
     try {
       const authCallbackString = localStorage.getItem("auth-callback");
@@ -14,6 +20,21 @@ export class NodeAuthCallbackCacheRepository implements GetAuthCallbackCacheRepo
       console.error(error);
 
       return null;
+    }
+  }
+
+  set(request: SetAuthCallbackCacheRepository.Request): SetAuthCallbackCacheRepository.Response {
+    try {
+      const authCallbackString = JSON.stringify(request);
+
+      localStorage.setItem("auth-callback", authCallbackString);
+
+      return true;
+    } catch (error) {
+      /* eslint-disable-next-line no-console */
+      console.error(error);
+
+      return false;
     }
   }
 }
