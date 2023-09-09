f538317db5c8aaede0e20a17cc6b8f1374589817
Nathan Murillo <nathanmurillodeoliveira@gmail.com>
2023-09-08 21:09:52 -0300

    feat: make GoogleAuthButton receive href property

--- src/presentation/components/GoogleAuthButton/GoogleAuthButton.tsx
+++ src/presentation/components/GoogleAuthButton/GoogleAuthButton.tsx
@@ -1,19 +1,34 @@
 import { Button } from "@mantine/core";
-import { ReactNode } from "react";
+import {
+  ReactNode,
+  CSSProperties
+} from "react";
 
 import { GoogleIcon } from "./GoogleIcon";
 
 export type GoogleAuthButtonProps = {
+  href: string;
   children?: ReactNode;
 };
 
 export const GoogleAuthButton = (props: GoogleAuthButtonProps) => {
-  const { children } = props;
+  const {
+    href,
+    children
+  } = props;
+
+  const linkStyle: CSSProperties = {
+    color: "inherit",
+    textDecoration: "none"
+  };
 
   const buttonIcon = <GoogleIcon />;
 
   return (
-    <>
+    <a
+      href={href}
+      style={linkStyle}
+    >
       <Button
         variant="default"
         color="gray"
@@ -22,6 +37,6 @@ export const GoogleAuthButton = (props: GoogleAuthButtonProps) => {
       >
         { children }
       </Button>
-    </>
+    </a>
   );
 };
