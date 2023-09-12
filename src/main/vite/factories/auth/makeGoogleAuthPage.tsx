aa2448639a34ba59d534dfa4e3b50390e4aa06d9
Nathan Murillo <nathanmurillodeoliveira@gmail.com>
2023-09-12 17:48:30 -0300

    refactor: remove redirect url creation from makeGoogleAuthPage

--- src/main/vite/factories/auth/makeGoogleAuthPage.tsx
+++ src/main/vite/factories/auth/makeGoogleAuthPage.tsx
@@ -1,4 +1,6 @@
 import { GoogleAuthPage } from "@/presentation/pages";
+import { GetAuthCallbackImpl } from "@/data/usecases";
+import { NodeAuthCallbackCacheRepository } from "@/infra/repositories";
 import {
   GOOGLE_OAUTH_BASE_URL,
   GOOGLE_OAUTH_CLIENT_ID,
@@ -7,21 +9,22 @@ import {
 } from "@/main/vite/config";
 
 export const makeGoogleAuthPage = () => {
-  const redirectURLBuilder = new URL(
-    GOOGLE_OAUTH_REDIRECT_URL_PATH,
-    window.location.origin
+  const getAuthCallbackRepository = new NodeAuthCallbackCacheRepository();
+  const getAuthCallback = new GetAuthCallbackImpl(
+    getAuthCallbackRepository
   );
-
-  const redirectURL = redirectURLBuilder.toString();
 
   const googleOAuth = {
     baseURL: GOOGLE_OAUTH_BASE_URL,
     clientId: GOOGLE_OAUTH_CLIENT_ID,
-    redirectURL: redirectURL,
+    redirectURLPath: GOOGLE_OAUTH_REDIRECT_URL_PATH,
     scopes: GOOGLE_OAUTH_SCOPES
   };
 
   return (
-    <GoogleAuthPage googleOAuth={googleOAuth} />
+    <GoogleAuthPage
+      getAuthCallback={getAuthCallback}
+      googleOAuth={googleOAuth}
+    />
   );
 };
