868a80e9aee2b503c6b1b2ffb8093f3ed705b04e
Nathan Murillo <nathanmurillodeoliveira@gmail.com>
2023-09-12 17:43:35 -0300

    fix: make GoogleAuthButton redirect after updating load state

--- src/presentation/components/GoogleAuthButton/GoogleAuthButton.tsx
+++ src/presentation/components/GoogleAuthButton/GoogleAuthButton.tsx
@@ -14,7 +14,21 @@ export const GoogleAuthButton = (props: GoogleAuthButtonProps) => {
 
   const handleClick = () => {
     setIsLoading(true);
-    window.location.href = href;
+
+    /*
+      Motivation:
+      When manually changing the current URL, Tauri WebView will freeze the page and not properly update its state.
+      This little delay is a workaround so that the user can have some feedback about the button click.
+      Despite that, the loading spinner animation will freeze too.
+    */
+    const delayInMilliseconds = 50;
+    setTimeout(
+      () => {
+        window.location.href = href;
+      },
+
+      delayInMilliseconds
+    );
   };
 
   const buttonIcon = <GoogleIcon />;
