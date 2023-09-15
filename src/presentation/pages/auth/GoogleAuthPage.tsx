8eea7161b007fd74784e2afd18ce0632a9a2c55e
Nathan Murillo <nathanmurillodeoliveira@gmail.com>
2023-09-15 13:24:55 -0300

    feat: make GoogleAuthPage receive GetAuthProfile and RemoveAuthCallback

--- src/presentation/pages/auth/GoogleAuthPage.tsx
+++ src/presentation/pages/auth/GoogleAuthPage.tsx
@@ -2,13 +2,26 @@ import {
   Paper,
   Container
 } from "@mantine/core";
+import {
+  useState,
+  useEffect
+} from "react";
 
-import { GetAuthCallback } from "@/domain/usecases";
+import {
+  GoogleAuthButton,
+  AuthenticatedButton
+} from "@/presentation/components";
+import {
+  GetAuthCallback,
+  GetAuthProfile,
+  RemoveAuthCallback
+} from "@/domain/usecases";
 import { BaseLayout } from "@/presentation/layouts";
-import { GoogleAuthButton } from "@/presentation/components";
 
 export type GoogleAuthPageProps = {
   getAuthCallback: GetAuthCallback;
+  getAuthProfile: GetAuthProfile;
+  removeAuthCallback: RemoveAuthCallback;
   googleOAuth: {
     baseURL: string;
     clientId: string;
@@ -20,13 +33,19 @@ export type GoogleAuthPageProps = {
 export const GoogleAuthPage = (props: GoogleAuthPageProps) => {
   const {
     getAuthCallback,
+    getAuthProfile,
+    removeAuthCallback,
     googleOAuth
   } = props;
+
+  type GetAuthProfileResponse = Awaited<ReturnType<GetAuthProfile["execute"]>>;
+
+  const [isLoadingAuthProfile, setIsLoadingAuthProfile] = useState(false);
+  const [authProfile, setAuthProfile] = useState<GetAuthProfileResponse>(null);
 
   const authCallback = getAuthCallback.execute();
 
   const clientBaseURL = window.location.origin;
-
   const redirectURLBuilder = new URL(
     googleOAuth.redirectURLPath,
     clientBaseURL.startsWith("tauri")
@@ -45,6 +64,83 @@ export const GoogleAuthPage = (props: GoogleAuthPageProps) => {
 
   const googleAuthenticationURL = googleAuthenticationURLBuilder.toString();
 
+  const handleLogout = () => {
+    removeAuthCallback.execute();
+    window.location.reload();
+  };
+
+  useEffect(
+    () => {
+      if (authCallback) {
+        setIsLoadingAuthProfile(true);
+
+        const getAuthProfileRequest = {
+          token: authCallback.accessToken
+        };
+
+        const handleGetAuthProfile = async (authProfile: GetAuthProfileResponse) => {
+          setAuthProfile(authProfile);
+          setIsLoadingAuthProfile(false);
+        };
+
+        /* eslint-disable no-console */
+        getAuthProfile.execute(getAuthProfileRequest)
+          .then(handleGetAuthProfile)
+          .catch(console.error);
+        /* eslint-enable no-console */
+      }
+    },
+    []
+  );
+
+  const paperPadding = !authCallback
+    ? 30
+    : 0;
+
+  type AuthPageButtonProps = {
+    hasAuthCallback: boolean;
+    isLoadingAuthProfile: boolean;
+    hasAuthProfile: boolean;
+  }
+
+  const AuthPageButton = (props: AuthPageButtonProps) => {
+    const {
+      hasAuthCallback,
+      isLoadingAuthProfile,
+      hasAuthProfile
+    } = props;
+
+    if (!hasAuthCallback)
+      return (
+        <>
+          <GoogleAuthButton href={googleAuthenticationURL} />
+        </>
+      );
+
+    if (isLoadingAuthProfile)
+      return (
+        <AuthenticatedButton>
+          <AuthenticatedButton.Loading />
+        </AuthenticatedButton>
+      );
+
+    if (!hasAuthProfile)
+      return (
+        <AuthenticatedButton handleLogout={handleLogout}>
+          <AuthenticatedButton.Error />
+        </AuthenticatedButton>
+      );
+
+    return (
+      <AuthenticatedButton handleLogout={handleLogout}>
+        <AuthenticatedButton.Done
+          name={authProfile!.name}
+          imageURL={authProfile!.imageURL}
+        />
+      </AuthenticatedButton>
+    );
+  };
+
   return (
     <BaseLayout activeOptionId="auth">
       <Container
@@ -54,15 +150,15 @@ export const GoogleAuthPage = (props: GoogleAuthPageProps) => {
         <Paper
           shadow="md"
           radius="md"
-          p={30}
           mt={30}
+          p={paperPadding}
           withBorder
         >
-          {
-            !authCallback
-              ? <GoogleAuthButton href={googleAuthenticationURL} />
-              : <div>Authenticated with Google</div>
-          }
+          <AuthPageButton
+            hasAuthCallback={!!authCallback}
+            isLoadingAuthProfile={isLoadingAuthProfile}
+            hasAuthProfile={!!authProfile}
+          />
         </Paper>
       </Container>
     </BaseLayout>
