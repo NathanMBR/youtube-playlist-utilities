33aa8706460531cc9422fd52bbf4f9b501826ca4
Nathan Murillo <nathanmurillodeoliveira@gmail.com>
2023-09-21 11:58:06 -0300

    feat: export YOUTUBE_BASE_PLAYLIST_ITEMS_URL

--- src/main/vite/config/env.ts
+++ src/main/vite/config/env.ts
@@ -4,3 +4,4 @@ export const GOOGLE_OAUTH_BASE_URL = String(import.meta.env.VITE_GOOGLE_OAUTH_BA
 export const GOOGLE_OAUTH_REDIRECT_URL = String(import.meta.env.VITE_GOOGLE_OAUTH_REDIRECT_URL);
 export const GOOGLE_OAUTH_SCOPES = String(import.meta.env.VITE_GOOGLE_OAUTH_SCOPES);
 export const GOOGLE_OAUTH_USER_INFO_URL = String(import.meta.env.VITE_GOOGLE_OAUTH_USER_INFO_URL);
+export const YOUTUBE_BASE_PLAYLIST_ITEMS_URL = String(import.meta.env.VITE_YOUTUBE_BASE_PLAYLIST_ITEMS_URL);
