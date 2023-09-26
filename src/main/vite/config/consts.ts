101845885246fd558649f6f7f7742c73961a5cab
Nathan Murillo <nathanmurillodeoliveira@gmail.com>
2023-09-25 22:16:50 -0300

    refactor: move many environment variables to constants file

---
+++ src/main/vite/config/consts.ts
@@ -0,0 +1,6 @@
+export const GITHUB_REPOSITORY_URL="https://github.com/NathanMBR/youtube-playlist-utilities";
+export const GOOGLE_OAUTH_BASE_URL="https://accounts.google.com/o/oauth2/v2/auth";
+export const GOOGLE_OAUTH_REDIRECT_URL="http://localhost:4172/auth/callback/rust";
+export const GOOGLE_OAUTH_SCOPES="https://www.googleapis.com/auth/userinfo.profile https://www.googleapis.com/auth/youtube";
+export const GOOGLE_OAUTH_USER_INFO_URL="https://www.googleapis.com/userinfo/v2/me";
+export const YOUTUBE_BASE_PLAYLIST_ITEMS_URL="https://www.googleapis.com/youtube/v3/playlistItems";
--- src/main/vite/config/env.ts
+++ src/main/vite/config/env.ts
@@ -1,7 +1,1 @@
-export const GITHUB_REPOSITORY_URL = String(import.meta.env.VITE_GITHUB_REPOSITORY_URL);
 export const GOOGLE_OAUTH_CLIENT_ID = String(import.meta.env.VITE_GOOGLE_OAUTH_CLIENT_ID);
-export const GOOGLE_OAUTH_BASE_URL = String(import.meta.env.VITE_GOOGLE_OAUTH_BASE_URL);
-export const GOOGLE_OAUTH_REDIRECT_URL = String(import.meta.env.VITE_GOOGLE_OAUTH_REDIRECT_URL);
-export const GOOGLE_OAUTH_SCOPES = String(import.meta.env.VITE_GOOGLE_OAUTH_SCOPES);
-export const GOOGLE_OAUTH_USER_INFO_URL = String(import.meta.env.VITE_GOOGLE_OAUTH_USER_INFO_URL);
-export const YOUTUBE_BASE_PLAYLIST_ITEMS_URL = String(import.meta.env.VITE_YOUTUBE_BASE_PLAYLIST_ITEMS_URL);
